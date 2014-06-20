83f209d8252e66d3c121590d315fb19dd7bdaa90
yuuuzuuu <kimurayuma@kimura-no-iMac.local>
2014-06-20 10:32:09 +0900

    Initial Commit

---
+++ The dog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  The dog
+//
+//  Created by 木村 友真 on 2014/06/20.
+//  Copyright (c) 2014年 yuma kimura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ The dogTests/The_dogTests.m
@@ -0,0 +1,34 @@
+//
+//  The_dogTests.m
+//  The dogTests
+//
+//  Created by 木村 友真 on 2014/06/20.
+//  Copyright (c) 2014年 yuma kimura. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface The_dogTests : XCTestCase
+
+@end
+
+@implementation The_dogTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
